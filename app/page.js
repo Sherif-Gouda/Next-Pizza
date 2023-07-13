a717640ff4ff266ccfbb6509beac0238085d94fb
Sherif Alaa <sherif.a.gouda@gmail.com>
2023-07-13 15:52:51 +0300

    fix responsiveness issues

--- app/page.js
+++ app/page.js
@@ -4,17 +4,17 @@ import Link from 'next/link'
 
 const Home = () => {
   return (
-    <div className='flex justify-between w-full px-52 items-center'>
-      <div className='flex flex-col items-center'>
-        <h1 className='text-6xl font-bold uppercase tracking-wide text-blue-500'>The best Pizza</h1>
-        <p className='font-semibold tracking-wide text-2xl mt-2'>Fresh from the oven, straight to your door!</p>
-        <Link href="/menu" className='bg-gray-900 rounded-lg text-white w-32 h-8 mt-6 text-center p-1 uppercase text-semibold'>Order now</Link>
-      </div>
+    <div className='flex flex-col justify-center items-center'>
       <div className='shrink-0'>
         <Image src={"https://pngimg.com/uploads/pizza/pizza_PNG44095.png"}
        className="drop-shadow-lg"
-       width={500}
-       height={500}/>
+       width={300}
+       height={300}/>
+      </div>
+      <div className='flex flex-col items-center mt-10'>
+        <h1 className='text-4xl md:text-6xl font-bold uppercase tracking-wide text-blue-500'>The best Pizza</h1>
+        <p className='font-semibold tracking-wide text:lg md:text-2xl mt-2'>Fresh from the oven, straight to your door!</p>
+        <Link href="/menu" className='bg-gray-900 rounded-lg text-white w-32 h-8 mt-6 text-center p-1 uppercase text-semibold'>Order now</Link>
       </div>
     </div>
   )
--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <nav> 
-        <div className="flex justify-between w-full px-12 pt-5 mb-16">
+        <div className="flex justify-between w-full px-4 pt-5 mb-16">
             <Link href="/" className='text-xl tracking-widest font-semibold uppercase color'>Next Pizza</Link>
-            <div className='flex justify-between w-1/12'>
+            <div className='flex justify-between w-2/5 md:w-2/12 items-center'>
                 <Link href="/menu">Menu</Link>
                 <Link href="/cart">Cart</Link>
                 <p>7erio</p>
