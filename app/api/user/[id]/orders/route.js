cd842413acb66a1885d6c4987f6431ac12d1ca21
Sherif Alaa <sherif.a.gouda@gmail.com>
2023-07-18 04:49:07 +0300

    Created the order schema and api endpoints

---
+++ app/api/user/[id]/orders/route.js
@@ -0,0 +1,31 @@
+import Order from "@/models/order";
+import { connectDB } from "@/utils/database";
+import { NextResponse } from "next/server";
+
+
+
+export const GET = async (request, {params})=>{
+    try {
+        await connectDB()
+        const id = params.id
+        const userOrders = await Order.find({user: id}).populate("items.item")
+        return new Response(JSON.stringify(userOrders))
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({success: false})
+    }
+}
+
+export const POST = async (request, {params})=>{
+    try {
+        await connectDB()
+        const id = params.id
+        const order = await request.json()
+        const newOrder = new Order({user: id, ...order})
+        await newOrder.save()
+        return NextResponse.json({success: true})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({success: false})
+    }
+}
---
+++ models/order.js
@@ -0,0 +1,27 @@
+import { Schema, model, models } from "mongoose";
+
+const orderSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    items: {type: [{
+        item: {
+            type: Schema.Types.ObjectId,
+            ref: 'MenuItem'
+        },
+        quantity: Number
+    
+    }],
+    required: true
+
+},
+
+    total: Number
+})
+
+const Order = models.Order || model("Order", orderSchema)
+
+export default Order
